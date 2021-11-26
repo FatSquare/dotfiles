807b27b89b84f2819b40e09be5afe6f813bb872e
FatSquare <legitcsgo5@gmail.com>
2021-11-26 10:45:04 +0100

    kitty/fish/i3/doom-emacs/

---
+++ .config/fish/config.fish
@@ -0,0 +1,23 @@
+## GREEITNG
+
+function fish_greeting
+#         paleofetch
+#         echo "UwU"
+end
+
+### Config
+#set fish_color_search_match --background='purple'
+#set fish_color_search_match green
+
+
+
+## ALIAS
+
+alias gpu="git pull"
+alias ga="git add ."
+alias gp="git push"
+alias gc="git commit -m"
+alias vps="ssh root@squar3.xyz"
+alias ls="exa -l --icons"
+alias vps-gover="ssh root@Dessertsacademy.com"
+alias emacs="emacsclient -c -a 'emacs'"
