dca94cc91042e4cd21ece967ad09f3597ff81cb1
Squar3 <69780128+FatSquare@users.noreply.github.com>
2021-12-11 21:26:58 +0100

    webh (fuck /usr/bin/)

--- .config/webh/app.py
+++ .config/webh/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    ost = os.popen('sh /usr/bin/battery')
+    ost = os.popen("upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep 'percentage' | awk '{print $2}'")
     return render_template('index.html',bt=ost.read())
 
 app.run(port=1111,debug=True)
